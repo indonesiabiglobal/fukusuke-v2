cad30e7a6d265cef301ab4c6e84268749a64d028
RafliFerdian25 <ferdianrafli25@gmail.com>
2024-07-29 22:28:27 +0700

    `feat` jenis produk

---
+++ app/Http/Livewire/MasterTabel/Produk/JenisProduk.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Livewire\MasterTabel\Produk;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Livewire\Component;
+
+class JenisProduk extends Component
+{
+    public $groupProducts;
+    public $searchTerm;
+    public $code;
+    public $name;
+    public $idUpdate;
+    public $idDelete;
+
+    public function mount()
+    {
+        $this->groupProducts = DB::table('msproduct_group')
+            ->where('status', 1)
+            ->get(['id', 'code', 'name', 'status', 'updated_by', 'updated_on']);
+    }
+
+    public function resetFields()
+    {
+        $this->code = '';
+        $this->name = '';
+    }
+
+    public function showModalCreate()
+    {
+        $this->resetFields();
+        $this->dispatch('showModalCreate');
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'code' => 'required|unique:msproduct_group,code',
+            'name' => 'required',
+        ]);
+
+        DB::beginTransaction();
+        try {
+            $statusActive = 1;
+            DB::table('msproduct_group')->insert([
+                'code' => $this->code,
+                'name' => $this->name,
+                'status' => $statusActive,
+                'created_by' => auth()->user()->username,
+                'created_on' => Carbon::now(),
+                'updated_by' => auth()->user()->username,
+                'updated_on' => Carbon::now(),
+            ]);
+            DB::commit();
+            $this->resetFields();
+            $this->search();
+            $this->dispatch('closeModalCreate');
+            $this->dispatch('notification', ['type' => 'success', 'message' => 'Master Group Product saved successfully.']);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error('Failed to save master Group Product: ' . $e->getMessage());
+            $this->dispatch('notification', ['type' => 'error', 'message' => 'Failed to save the Group Product: ' . $e->getMessage()]);
+        }
+    }
+
+    public function edit($id)
+    {
+        $groupProduct = DB::table('msproduct_group')->where('id', $id)->first();
+        $this->idUpdate = $id;
+        $this->code = $groupProduct->code;
+        $this->name = $groupProduct->name;
+        $this->dispatch('showModalUpdate');
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'code' => 'required|unique:msproduct_group,code,' . $this->idUpdate,
+            'name' => 'required',
+        ]);
+
+        DB::beginTransaction();
+        try {
+            DB::table('msproduct_group')->where('id', $this->idUpdate)->update([
+                'code' => $this->code,
+                'name' => $this->name,
+                'updated_by' => auth()->user()->username,
+                'updated_on' => Carbon::now(),
+            ]);
+            DB::commit();
+            $this->resetFields();
+            $this->search();
+            $this->dispatch('closeModalUpdate');
+            $this->dispatch('notification', ['type' => 'success', 'message' => 'Master Group Product updated successfully.']);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error('Failed to save master Group Product: ' . $e->getMessage());
+            $this->dispatch('notification', ['type' => 'error', 'message' => 'Failed to save the Group Product: ' . $e->getMessage()]);
+        }
+    }
+
+    public function delete($id)
+    {
+        $this->idDelete = $id;
+        $this->dispatch('showModalDelete');
+    }
+
+    public function destroy()
+    {
+        DB::beginTransaction();
+        try {
+            DB::table('msproduct_group')->where('id', $this->idDelete)->delete();
+            DB::commit();
+            $this->search();
+            $this->dispatch('closeModalDelete');
+            $this->dispatch('notification', ['type' => 'success', 'message' => 'Master Group Product deleted successfully.']);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error('Failed to delete master Group Product: ' . $e->getMessage());
+            $this->dispatch('notification', ['type' => 'error', 'message' => 'Failed to delete the Group Product: ' . $e->getMessage()]);
+        }
+    }
+
+    public function search()
+    {
+        $this->groupProducts = DB::table('msproduct_group')
+            ->when(isset($this->searchTerm) && $this->searchTerm != "" && $this->searchTerm != "undefined", function ($query) {
+                $query->where(function ($queryWhere) {
+                    $queryWhere->where('code', 'ilike', "%" . $this->searchTerm . "%")
+                        ->orWhere('name', 'ilike', "%" . $this->searchTerm . "%");
+                });
+            })
+            ->where('status', 1)
+            ->get(['id', 'code', 'name', 'status', 'updated_by', 'updated_on']);
+
+        $this->render();
+    }
+
+    public function render()
+    {
+        return view('livewire.master-tabel.produk.jenis-produk')->extends('layouts.master');
+    }
+}
---
+++ resources/views/livewire/master-tabel/produk/jenis-produk.blade.php
@@ -0,0 +1,328 @@
+<div class="row">
+    <div class="col-lg-12 mt-2">
+        <div class="row">
+
+            <div class="col-12 col-lg-6">
+                <button class="btn btn-primary w-lg p-1" wire:click="download" type="button">
+                    <span wire:loading.remove wire:target="download">
+                        <i class="ri-download-cloud-2-line"> </i> Download
+                    </span>
+                    <div wire:loading wire:target="download">
+                        <span class="d-flex align-items-center">
+                            <span class="spinner-border flex-shrink-0" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                            </span>
+                            <span class="flex-grow-1 ms-1">
+                                Loading...
+                            </span>
+                        </span>
+                    </div>
+                </button>
+                <button class="btn btn-info w-lg p-1" wire:click="print" type="button">
+                    <span wire:loading.remove wire:target="print">
+                        <i class="ri-printer-line"> </i> Print
+                    </span>
+                    <div wire:loading wire:target="print">
+                        <span class="d-flex align-items-center">
+                            <span class="spinner-border flex-shrink-0" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                            </span>
+                            <span class="flex-grow-1 ms-1">
+                                Loading...
+                            </span>
+                        </span>
+                    </div>
+                </button>
+                {{-- Button Add groupProduct --}}
+                <button type="button" class="btn btn-success w-lg p-1" wire:click="showModalCreate">
+                    <i class="ri-add-line"> </i> Add
+                </button>
+                {{-- modal add groupProduct --}}
+                <div class="modal fade" id="modal-add" tabindex="-1" aria-labelledby="modal-addLabel" aria-modal="true"
+                    wire:ignore.self>
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="modal-addLabel">Add Master Jenis Produk</h5> <button
+                                    type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form wire:submit.prevent="store">
+                                    <div class="row g-3">
+                                        {{-- kode groupProduct --}}
+                                        <div class="col-xxl-12">
+                                            <div>
+                                                <label for="code" class="form-label">Kode Jenis Produk</label>
+                                                <input type="number"
+                                                    class="form-control @error('code') is-invalid @enderror"
+                                                    id="code" wire:model.defer="code" placeholder="Kode">
+                                                @error('code')
+                                                    <span class="invalid-feedback">{{ $message }}</span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                        {{-- nama groupProduct --}}
+                                        <div class="col-xxl-12">
+                                            <div>
+                                                <label for="name" class="form-label">Nama Jenis Produk</label>
+                                                <input type="text"
+                                                    class="form-control @error('name') is-invalid @enderror"
+                                                    id="name" wire:model.defer="name" placeholder="Nama">
+                                                @error('name')
+                                                    <span class="invalid-feedback">{{ $message }}</span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                        {{-- button --}}
+                                        <div class="col-lg-12">
+                                            <div class="hstack gap-2 justify-content-end">
+                                                <button type="button" class="btn btn-light"
+                                                    data-bs-dismiss="modal">Close</button>
+                                                <button id="btnCreate" type="submit" class="btn btn-success w-lg">
+                                                    <span wire:loading.remove wire:target="store">
+                                                        <i class="ri-save-3-line"></i> Save
+                                                    </span>
+                                                    <div wire:loading wire:target="store">
+                                                        <span class="d-flex align-items-center">
+                                                            <span class="spinner-border flex-shrink-0" role="status">
+                                                                <span class="visually-hidden">Loading...</span>
+                                                            </span>
+                                                            <span class="flex-grow-1 ms-1">
+                                                                Loading...
+                                                            </span>
+                                                        </span>
+                                                    </div>
+                                                </button>
+                                                {{-- <button type="submit" class="btn btn-primary">Save</button> --}}
+                                            </div>
+                                        </div><!--end col-->
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- end modal groupProduct --}}
+
+                {{-- modal add groupProduct --}}
+                <div class="modal fade" id="modal-edit" tabindex="-1" aria-labelledby="modal-editLabel"
+                    aria-modal="true" wire:ignore.self>
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="modal-editLabel">Edit Master Jenis Produk</h5> <button
+                                    type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form wire:submit.prevent="update">
+                                    <div class="row g-3">
+                                        {{-- kode groupProduct --}}
+                                        <div class="col-xxl-12">
+                                            <div>
+                                                <label for="code" class="form-label">Kode Jenis Produk</label>
+                                                <input type="number"
+                                                    class="form-control @error('code') is-invalid @enderror"
+                                                    id="code" wire:model.defer="code" placeholder="Kode">
+                                                @error('code')
+                                                    <span class="invalid-feedback">{{ $message }}</span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                        {{-- nama groupProduct --}}
+                                        <div class="col-xxl-12">
+                                            <div>
+                                                <label for="name" class="form-label">Nama Jenis Produk</label>
+                                                <input type="text"
+                                                    class="form-control @error('name') is-invalid @enderror"
+                                                    id="name" wire:model.defer="name" placeholder="Nama">
+                                                @error('name')
+                                                    <span class="invalid-feedback">{{ $message }}</span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                        {{-- button --}}
+                                        <div class="col-lg-12">
+                                            <div class="hstack gap-2 justify-content-end">
+                                                <button type="button" class="btn btn-light"
+                                                    data-bs-dismiss="modal">Close</button>
+                                                <button id="btnCreate" type="submit" class="btn btn-success w-lg">
+                                                    <span wire:loading.remove wire:target="update">
+                                                        <i class="ri-save-3-line"></i> Update
+                                                    </span>
+                                                    <div wire:loading wire:target="update">
+                                                        <span class="d-flex align-items-center">
+                                                            <span class="spinner-border flex-shrink-0" role="status">
+                                                                <span class="visually-hidden">Loading...</span>
+                                                            </span>
+                                                            <span class="flex-grow-1 ms-1">
+                                                                Loading...
+                                                            </span>
+                                                        </span>
+                                                    </div>
+                                                </button>
+                                                {{-- <button type="submit" class="btn btn-primary">Save</button> --}}
+                                            </div>
+                                        </div><!--end col-->
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- end modal groupProduct --}}
+
+
+                {{-- start modal delete groupProduct --}}
+                <div id="removeGroupProductModal" class="modal fade zoomIn" tabindex="-1" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+                                    id="close-removeGroupProductModal"></button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="mt-2 text-center">
+                                    <lord-icon src="https://cdn.lordicon.com/gsqxdxog.json" trigger="loop"
+                                        colors="primary:#f7b84b,secondary:#f06548"
+                                        style="width:100px;height:100px"></lord-icon>
+                                    <div class="mt-4 pt-2 fs-15 mx-4 mx-sm-5">
+                                        <h4>Are you sure ?</h4>
+                                        <p class="text-muted mx-4 mb-0">Are you sure you want to remove this group
+                                            Product ?
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="d-flex gap-2 justify-content-center mt-4 mb-2">
+                                    <button type="button" class="btn w-sm btn-light"
+                                        data-bs-dismiss="modal">Close</button>
+                                    <button wire:click="destroy" id="btnCreate" type="button"
+                                        class="btn w-sm btn-danger" id="remove-item">
+                                        <span wire:loading.remove wire:target="destroy">
+                                            <i class="ri-save-3-line"></i> Yes, Delete It!
+                                        </span>
+                                        <div wire:loading wire:target="destroy">
+                                            <span class="d-flex align-items-center">
+                                                <span class="spinner-border flex-shrink-0" role="status">
+                                                    <span class="visually-hidden">Loading...</span>
+                                                </span>
+                                                <span class="flex-grow-1 ms-1">
+                                                    Loading...
+                                                </span>
+                                            </span>
+                                        </div>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- end modal delete groupProduct --}}
+            </div>
+            <div class="col-12 col-lg-6">
+                <form wire:submit.prevent="search">
+                <div class="input-group">
+                        <input wire:model.defer="searchTerm" class="form-control"style="padding:0.44rem"
+                            type="text" placeholder="search nama jenis produk" />
+                        <button wire:click="search" type="submit" class="btn btn-primary btn-load w-lg p-1">
+                            <span wire:loading.remove wire:target="search">
+                                <i class="ri-search-line"></i> Filter
+                            </span>
+                            <div wire:loading wire:target="search">
+                                <span class="d-flex align-items-center">
+                                    <span class="spinner-border flex-shrink-0" role="status">
+                                        <span class="visually-hidden">Loading...</span>
+                                    </span>
+                                    <span class="flex-grow-1 ms-1">
+                                        Loading...
+                                    </span>
+                                </span>
+                            </div>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="table-responsive table-card mt-3 mb-1">
+        <table class="table align-middle table-nowrap" id="customerTable" style="width:100%">
+            <thead class="table-light">
+                <tr>
+                    <th>Action</th>
+                    <th class="sort">Kode Jenis Produk</th>
+                    <th class="sort">Nama Jenis Produk</th>
+                    <th class="sort">Status</th>
+                    <th class="sort">Updated By</th>
+                    <th class="sort">Updated</th>
+                    {{-- <th class="sort">No.</th> --}}
+                </tr>
+            </thead>
+            <tbody class="list form-check-all">
+                @forelse ($groupProducts as $item)
+                    <tr>
+                        <td>
+                            <button type="button" class="btn fs-15 p-1 bg-primary rounded" data-bs-toggle="modal"
+                                data-bs-target="#modal-edit" wire:click="edit({{ $item->id }})">
+                                <i class="ri-edit-box-line text-white"></i>
+                            </button>
+                            <button type="button" class="btn fs-15 p-1 bg-danger rounded removeGroupProductModal"
+                                wire:click="delete({{ $item->id }})">
+                                <i class="ri-delete-bin-line  text-white"></i>
+                            </button>
+                        </td>
+                        <td>{{ $item->code }}</td>
+                        <td>{{ $item->name }}</td>
+                        <td>
+                            {!! $item->status == 1
+                                ? '<span class="badge text-success bg-success-subtle">Active</span>'
+                                : '<span class="badge text-bg-danger">Non Active</span>' !!}
+                        </td>
+                        <td>{{ $item->updated_by }}</td>
+                        <td>{{ $item->updated_on }}</td>
+                        {{-- <td>{{ $no++ }}</td> --}}
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="10" class="text-center">
+                            <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop"
+                                colors="primary:#121331,secondary:#08a88a" style="width:40px;height:40px"></lord-icon>
+                            <h5 class="mt-2">Sorry! No Result Found</h5>
+                            <p class="text-muted mb-0">We've searched more than 150+ Orders We did not find any orders
+                                for you search.</p>
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+        {{-- {{ $groupProducts->links() }} --}}
+    </div>
+    {{-- <livewire:tdorder/> --}}
+</div>
+
+@script
+    <script>
+        $wire.on('showModalCreate', () => {
+            $('#modal-add').modal('show');
+        });
+        // close modal create groupProduct
+        $wire.on('closeModalCreate', () => {
+            $('#modal-add').modal('hide');
+        });
+
+        // close modal update groupProduct
+        $wire.on('closeModalUpdate', () => {
+            $('#modal-edit').modal('hide');
+        });
+
+        // show modal delete groupProduct
+        $wire.on('showModalDelete', () => {
+            $('#removeGroupProductModal').modal('show');
+        });
+
+        // close modal delete groupProduct
+        $wire.on('closeModalDelete', () => {
+            $('#removeGroupProductModal').modal('hide');
+        });
+    </script>
+@endscript
