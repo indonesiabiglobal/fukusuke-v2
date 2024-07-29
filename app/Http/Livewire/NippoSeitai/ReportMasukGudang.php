b7e92840c008bb1276aa2f01560f3fd73655ed90
richo15 <gre.richo@gmail.com>
2024-07-30 00:12:24 +0700

    label masuk

--- app/Http/Livewire/NippoSeitai/ReportMasukGudang.php
+++ app/Http/Livewire/NippoSeitai/ReportMasukGudang.php
@@ -6,8 +6,35 @@ use Livewire\Component;
 
 class ReportMasukGudang extends Component
 {
+    public $nomor_palet;
+
+
     public function render()
     {
+
+        $this->nomor_palet=$nomor_palet;
+
+        dd( $this->nomor_palet);
+        $data = collect(DB::select("
+        SELECT 
+        tdpg.production_date AS production_date, 
+        tdpg.nomor_palet AS nomor_palet, 
+            tdpg.nomor_lot AS nomor_lot,
+                tdpg.work_shift AS work_shift,  
+                tdpg.employee_id AS employee_id,
+                me.empname as namapetugas,
+                tdpg.product_id AS product_id,
+                mp.name as namaproduk,
+                mp.palet_jumlah_baris as tinggi,
+                mp.palet_isi_baris as jmlbaris,
+                tdpg.qty_produksi AS qty_produksi
+            FROM  tdProduct_Goods AS tdpg
+            left JOIN tdOrderLpk AS tdol ON tdpg.lpk_id = tdol.id
+                left join msproduct as mp on mp.id=tdpg.product_id
+                left join msemployee as me on me.id=tdpg.employee_id
+            WHERE tdpg.nomor_palet = (LTRIM(RTRIM('G2524-040724')))
+        "))->first();
+
         return view('livewire.nippo-seitai.report-masuk-gudang');
     }
 }
--- resources/views/livewire/nippo-seitai/report-masuk-gudang.blade.php
+++ resources/views/livewire/nippo-seitai/report-masuk-gudang.blade.php
@@ -6,10 +6,36 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-{{-- @php
-    header("Content-type: application/vnd-ms-excel");
-	header("Content-Disposition: attachment; filename=test.xls");
-@endphp --}}
+@php
+ use Carbon\Carbon;
+    // header("Content-type: application/vnd-ms-excel");
+	// header("Content-Disposition: attachment; filename=test.xls");
+    $data = collect(
+        DB::select("
+        SELECT 
+        tdpg.production_date AS production_date, 
+        tdpg.nomor_palet AS nomor_palet, 
+            tdpg.nomor_lot AS nomor_lot,
+                tdpg.work_shift AS work_shift,  
+                tdpg.employee_id AS employee_id,
+                me.empname as namapetugas,
+                tdpg.product_id AS product_id,
+                mp.code_alias as nocode,
+                mp.name as namaproduk,
+                mp.palet_jumlah_baris as tinggi,
+                mp.palet_isi_baris as jmlbaris,
+                tdpg.qty_produksi/cast(mp.case_box_count as  INTEGER) AS qty_produksi
+            FROM  tdProduct_Goods AS tdpg
+            left JOIN tdOrderLpk AS tdol ON tdpg.lpk_id = tdol.id
+                left join msproduct as mp on mp.id=tdpg.product_id
+                left join msemployee as me on me.id=tdpg.employee_id
+            WHERE tdpg.nomor_palet = (LTRIM(RTRIM('$no_palet')))
+        "));
+        //  dd($data[0]->namaproduk);
+        $parts = explode('-', $data[0]->nomor_palet);
+        $date = Carbon::parse( $data[0]->production_date);
+        // dd( );
+@endphp
 <body style="background-color: #CCCCCC;margin: 0">
     <div align="center">
         <table class="bayangprint" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" border="0" width="730" style="padding:25px">
@@ -26,7 +52,7 @@
                                 <td width="50%" align="center">
                                     <span>
                                         <font>Nomor : </font>
-                                        <font style="font-size: 22px; font-weight: bold;">02074</font>
+                                        <font style="font-size: 22px; font-weight: bold;">{{ $parts[1] }}</font>
                                     </span>
                                 </td>
                             </tr>
@@ -40,7 +66,7 @@
                                 </td>
                                 <td width="34%" style="border: 1px solid black;padding: 3px;">
                                     <span>
-                                        <font>Tanggal : 03/07/2024</font>
+                                        <font>Tanggal : {{ $date->format('d/m/Y') }}</font>
                                     </span>
                                 </td>
                                 <td width="5%"></td>
@@ -67,7 +93,7 @@
                                 <td width="5%"></td>
                                 <td width="50%" style="border: 1px dashed black;padding: 3px;">
                                     <span>
-                                        <font style="font-size: 55px;">G0141</font>
+                                        <font style="font-size: 55px;">{{ $parts[0] }}</font>
                                     </span>
                                 </td>
                             </tr>
@@ -93,7 +119,35 @@
                                                 <span>Shift</span>
                                             </td>
                                         </tr>
+                                        @php
+                                            $totalqty=0;
+                                        @endphp
+                                        @foreach ($data as $dt )
+                 
                                         <tr>
+                                            <td width="30%" style="border: 1px solid black;padding: 3px;">
+                                                <span>{{$dt->nomor_lot}}</span>
+                                            </td>
+                                            <td width="10%" style="border: 1px solid black;padding: 3px;">
+                                                <span>{{$dt->qty_produksi}}</span>
+                                            </td>
+                                            <td width="10%" style="border: 1px solid black;padding: 3px;">
+                                                <span></span>
+                                            </td>
+                                            <td width="30%" style="border: 1px solid black;padding: 3px;">
+                                                <span>{{$dt->namapetugas}}</span>
+                                            </td>
+                                            <td width="10%" style="border: 1px solid black;padding: 3px;">
+                                                <span>{{$dt->work_shift}}</span>
+                                            </td>
+                                        </tr>
+                                        @php
+                                            $totalqty=$totalqty + $dt->qty_produksi
+                                        @endphp
+                                        
+                                        @endforeach
+
+                                        {{-- <tr>
                                             <td width="30%" style="border: 1px solid black;padding: 3px;">
                                                 <span>96402217</span>
                                             </td>
@@ -245,24 +299,7 @@
                                             <td width="10%" style="border: 1px solid black;padding: 3px;">
                                                 <span>2</span>
                                             </td>
-                                        </tr>
-                                        <tr>
-                                            <td width="30%" style="border: 1px solid black;padding: 3px;">
-                                                <span>96402217</span>
-                                            </td>
-                                            <td width="10%" style="border: 1px solid black;padding: 3px;">
-                                                <span>0</span>
-                                            </td>
-                                            <td width="10%" style="border: 1px solid black;padding: 3px;">
-                                                <span></span>
-                                            </td>
-                                            <td width="30%" style="border: 1px solid black;padding: 3px;">
-                                                <span>Winda Rahayu</span>
-                                            </td>
-                                            <td width="10%" style="border: 1px solid black;padding: 3px;">
-                                                <span>2</span>
-                                            </td>
-                                        </tr>
+                                        </tr> --}}
                                     </table>
                                     <table width="100%" cellspacing="0" cellpadding="0" border="0">
                                         <tr>
@@ -279,19 +316,19 @@
                                         <tr>
                                             <td style="border: 1px solid black;padding: 10px;">
                                                 <span style="vertical-align: top;">Nomor Produk </span>
-                                                <font style="font-size: 33.2px;">3501</font>
+                                                <font style="font-size: 33.2px;">{{ $data[0]->nocode }}</font>
                                             </td>
                                         </tr>
                                         <tr>
                                             <td style="border: 1px solid black;padding: 3px;">
-                                                <span style="vertical-align: top;">Nama Produk </span><br>
-                                                <p style="font-size: 28px;text-align:center;">Seria Eiji 30</p>
+                                                <span style="vertical-align: top;">Nama Produk</span><br>
+                                                <p style="font-size: 24px;text-align:center;">{{ $data[0]->namaproduk }}</p>
                                             </td>
                                         </tr>
                                         <tr>
                                             <td style="border: 1px solid black;padding: 3px;">
                                                 <span style="vertical-align: top;">Jumlah Kotak (Box) </span>
-                                                <font style="font-size: 42px;font-weight:bold;">13</font>
+                                                <font style="font-size: 42px;font-weight:bold;">{{$totalqty}}</font>
                                             </td>
                                         </tr>
                                         <tr>
@@ -305,15 +342,15 @@
                                                 </table>
                                                 <table width="100%" cellspacing="0" cellpadding="0" border="0">
                                                     <tr>
-                                                        <td width="30%" style="text-align: center;font-size:30px;border: 1px solid black;">1</td>
+                                                        <td width="30%" style="text-align: center;font-size:30px;border: 1px solid black;">{{ $data[0]->tinggi }}</td>
                                                         <td width="5%" style="text-align: center;font-size:20px;">
                                                             &nbsp;X&nbsp; 
                                                         </td>
-                                                        <td width="30%" style="text-align: center;font-size:30px;border: 1px solid black;">8</td>
+                                                        <td width="30%" style="text-align: center;font-size:30px;border: 1px solid black;">{{ $data[0]->jmlbaris }}</td>
                                                         <td width="5%" style="text-align: center;font-size:20px;">
                                                             &nbsp;+&nbsp; 
                                                         </td>
-                                                        <td width="30%" style="text-align: center;font-size:30px;border: 1px solid black;">5</td>
+                                                        <td width="30%" style="text-align: center;font-size:30px;border: 1px solid black;">{{ $data[0]->jmlbaris }}</td>
                                                     </tr>
                                                 </table>
                                             </td>
