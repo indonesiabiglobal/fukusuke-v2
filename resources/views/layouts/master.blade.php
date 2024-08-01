bc1c5e44acdc3bb3b6ed8652001777cfa264986c
resna <resnapangestu01@gmail.com>
2024-08-02 03:52:26 +0700

    -

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,10 @@
 
 <head>
     <meta charset="utf-8" />
+    <!-- PWA  -->
+    <meta name="theme-color" content="#6777ef"/>
+    <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
     <title> Fukusuke </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
@@ -63,6 +67,23 @@
             var message = "{{ session('notification')['message'] }}";
             toastr[type](message);
         @endif
+    </script>
+    <script src="{{ asset('/sw.js') }}"></script>
+    <script>
+       if ("serviceWorker" in navigator) {
+          // Register a service worker hosted at the root of the
+          // site using the default scope.
+          navigator.serviceWorker.register("/sw.js").then(
+          (registration) => {
+             console.log("Service worker registration succeeded:", registration);
+          },
+          (error) => {
+             console.error(`Service worker registration failed: ${error}`);
+          },
+        );
+      } else {
+         console.error("Service workers are not supported.");
+      }
     </script>
 </body>
 
