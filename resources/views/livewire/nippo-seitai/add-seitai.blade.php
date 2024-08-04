0794f5b6307e74f929b5fc2f7250bde172c745d8
resna <resnapangestu01@gmail.com>
2024-08-04 12:43:46 +0700

    -

--- resources/views/livewire/nippo-seitai/add-seitai.blade.php
+++ resources/views/livewire/nippo-seitai/add-seitai.blade.php
@@ -434,7 +434,7 @@
                                     <div class="form-group">
                                         <label>Line Gentan </label>
                                         <div class="input-group col-md-9 col-xs-8">
-                                            <input class="form-control" type="text" wire:model.defer="gentan_line" placeholder="0" />
+                                            <input class="form-control" type="text" wire:model.defer="gentan_line" placeholder="Enter A atau B" />
                                             @error('gentan_line')
                                                 <span class="invalid-feedback">{{ $message }}</span>
                                             @enderror
