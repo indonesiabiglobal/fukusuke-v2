8977239d1631f050e88eb7dab78344d9fbd98f7b
letotong <resnapangestu01@gmail.com>
2024-08-12 10:15:08 +0700

    -

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
     <meta name="theme-color" content="#6777ef"/>
     {{-- <link rel="apple-touch-icon" href="{{ asset('logo.png') }}"> --}}
     <link rel="manifest" href="{{ asset('/manifest.json') }}">
-    <title> Fukusuke </title>
+    <title> Fukusuke Kogyo Indonesia </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
