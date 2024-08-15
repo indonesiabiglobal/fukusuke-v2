315ccb0f933a95ccd89978ae7e5bb44679042441
RafliFerdian25 <ferdianrafli25@gmail.com>
2024-08-15 23:26:58 +0700

    `feat` header detail report

--- app/Http/Livewire/Report/DetailReportController.php
+++ app/Http/Livewire/Report/DetailReportController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Livewire\Report;
 
 use App\Exports\DetailReportExport;
+use App\Helpers\phpspreadsheet;
 use App\Models\MsWorkingShift;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Livewire\Component;
 use Maatwebsite\Excel\Facades\Excel;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class DetailReportController extends Component
 {
@@ -15,6 +20,7 @@ class DetailReportController extends Component
     public $jamAwal;
     public $jamAkhir;
     public $workingShiftHour;
+    public $nippo = 'Infure';
 
     public function mount()
     {
@@ -31,7 +37,270 @@ class DetailReportController extends Component
         //     $this->tglAwal,
         //     $this->tglAkhir
         // ), 'Detail_Report.xlsx');
+
+        $rules = [
+            'tglAwal' => 'required',
+            'tglAkhir' => 'required',
+            'jamAwal' => 'required',
+            'jamAkhir' => 'required',
+            'nippo' => 'required',
+        ];
+
+        $messages = [
+            'tglAwal.required' => 'Tanggal Awal tidak boleh kosong',
+            'tglAkhir.required' => 'Tanggal Akhir tidak boleh kosong',
+            'jamAwal.required' => 'Jam Awal tidak boleh kosong',
+            'jamAkhir.required' => 'Jam Akhir tidak boleh kosong',
+            'nippo.required' => 'Jenis Report tidak boleh kosong',
+        ];
+
+        $validate = Validator::make([
+            'tglAwal' => $this->tglAwal,
+            'tglAkhir' => $this->tglAkhir,
+            'jamAwal' => $this->jamAwal,
+            'jamAkhir' => $this->jamAkhir,
+            'nippo' => $this->nippo,
+        ], $rules, $messages);
+
+        if ($validate->fails()) {
+            $this->dispatch('notification', ['type' => 'warning', 'message' => $validate->errors()->first()]);
+            return;
+        }
+
+        if ($this->tglAwal > $this->tglAkhir) {
+            $this->dispatch('notification', ['type' => 'warning', 'message' => 'Tanggal akhir tidak boleh kurang dari tanggal awal']);
+            return;
+        }
+
+        $tglAwal = Carbon::parse($this->tglAwal . ' ' . $this->jamAwal);
+        $tglAkhir = Carbon::parse($this->tglAkhir . ' ' . $this->jamAkhir);
+
+        if ($this->nippo == 'Infure') {
+            $response = $this->reportInfure($tglAwal, $tglAkhir);
+            if ($response['status'] == 'success') {
+                return response()->download($response['filename']);
+            } else if ($response['status'] == 'error') {
+                $this->dispatch('notification', ['type' => 'warning', 'message' => $response['message']]);
+                return;
+            }
+        } else if ($this->nippo == 'Seitai'){
+            $response = $this->reportSeitai($tglAwal, $tglAkhir);
+            if ($response['status'] == 'success') {
+                return response()->download($response['filename']);
+            } else if ($response['status'] == 'error') {
+                $this->dispatch('notification', ['type' => 'warning', 'message' => $response['message']]);
+                return;
+            }
+        }
     }
+
+    public function reportInfure($tglAwal, $tglAkhir)
+    {
+        $spreadsheet = new Spreadsheet();
+        $activeWorksheet = $spreadsheet->getActiveSheet();
+        $activeWorksheet->setShowGridlines(false);
+
+        // Set locale agar tanggal indonesia
+        Carbon::setLocale('id');
+
+        // Judul
+        $activeWorksheet->setCellValue('A1', 'DETAIL PRODUKSI INFURE');
+        $activeWorksheet->setCellValue('A2', 'Periode: ' . $tglAwal->translatedFormat('d-M-Y H:i') . ' s/d ' . $tglAkhir->translatedFormat('d-M-Y H:i'));
+        // Style Judul
+        phpspreadsheet::styleFont($spreadsheet, 'A1:A2', true, 11, 'Calibri');
+
+        // header
+        $rowHeaderStart = 3;
+        $columnHeaderStart = 'A';
+        $columnHeaderEnd = 'A';
+
+        $header = [
+            'Tanggal Produksi',
+            'Shift',
+            'Jam',
+            'NIK',
+            'Nama Petugas',
+            'Dept. Petugas',
+            'Mesin',
+            'No LPK',
+            'Nomor Gentan',
+            'Nomor Han',
+            'Panjang Produksi (meter)',
+            'Berat Produksi (Kg)',
+            'Loss',
+            'Berat Loss (Kg)',
+        ];
+
+        foreach ($header as $key => $value) {
+            $activeWorksheet->setCellValue($columnHeaderEnd . $rowHeaderStart, $value);
+            $columnHeaderEnd++;
+        }
+        $columnHeaderEnd = chr(ord($columnHeaderEnd) - 1);
+
+        // style header
+        phpspreadsheet::addFullBorder($spreadsheet, $columnHeaderStart . $rowHeaderStart . ':' . $columnHeaderEnd . $rowHeaderStart);
+        phpspreadsheet::styleFont($spreadsheet, $columnHeaderStart . $rowHeaderStart . ':' . $columnHeaderEnd . $rowHeaderStart, true, 9, 'Calibri');
+        phpspreadsheet::textAlignCenter($spreadsheet, $columnHeaderStart . $rowHeaderStart . ':' . $columnHeaderEnd . $rowHeaderStart);
+
+        // qeury belum bener
+        $data = collect(DB::select(
+            "
+                SELECT
+                    tod.id,
+                    tod.po_no,
+                    mp.code,
+                    mp.name AS produk_name,
+                    tod.product_code,
+                    tod.order_qty,
+                    tod.order_unit,
+                    tod.stufingdate,
+                    tod.etddate,
+                    tod.etadate,
+                    mbu.NAME AS buyer_name
+                FROM
+                    tdorder AS tod
+                INNER JOIN msproduct AS mp ON mp.id = tod.product_id
+                INNER JOIN msbuyer AS mbu ON mbu.id = tod.buyer_id",
+        ));
+
+        if (count($data) == 0) {
+            $response = [
+                'status' => 'error',
+                'message' => "Data pada periode tanggal atau pembeli tersebut tidak ditemukan"
+            ];
+
+            return $response;
+        }
+
+        $activeWorksheet->getStyle($columnHeaderStart . $rowHeaderStart . ':' . $columnHeaderEnd . $rowHeaderStart)->getAlignment()->setWrapText(true);
+
+        // size auto
+        // $columnSizeStart = $columnItemStart;
+        // $columnSizeStart++;
+        // while ($columnSizeStart !== $columnItemEnd) {
+        //     $spreadsheet->getActiveSheet()->getColumnDimension($columnSizeStart)->setAutoSize(true);
+        //     $columnSizeStart++;
+        // }
+
+        $writer = new Xlsx($spreadsheet);
+        $filename = 'Detail-Produksi-'. $this->nippo . '.xlsx';
+        $writer->save($filename);
+        $response = [
+            'status' => 'success',
+            'filename' => $filename
+        ];
+        return $response;
+    }
+
+    public function reportSeitai($tglAwal, $tglAkhir)
+    {
+        $spreadsheet = new Spreadsheet();
+        $activeWorksheet = $spreadsheet->getActiveSheet();
+        $activeWorksheet->setShowGridlines(false);
+
+        // Set locale agar tanggal indonesia
+        Carbon::setLocale('id');
+
+        // Judul
+        $activeWorksheet->setCellValue('A1', 'DETAIL PRODUKSI SEITAI');
+        $activeWorksheet->setCellValue('A2', 'Periode: ' . $tglAwal->translatedFormat('d-M-Y H:i') . ' s/d ' . $tglAkhir->translatedFormat('d-M-Y H:i'));
+        // Style Judul
+        phpspreadsheet::styleFont($spreadsheet, 'A1:A2', true, 11, 'Calibri');
+
+        // header
+        $rowHeaderStart = 3;
+        $columnHeaderStart = 'A';
+        $columnHeaderEnd = 'A';
+
+        $header = [
+            'Tanggal Produksi',
+            'Nama Petugas',
+            'Dept. Petugas',
+            'Nomor Mesin',
+            'No LPK',
+            'Shift',
+            'Jam',
+            'NIK',
+            'Nomor Palet',
+            'Nomor LOT',
+            'Quantity (Lembar)',
+            'Loss',
+            'Berat Loss (Kg)',
+            'Nomor Gentan',
+            'Panjang (meter)',
+            'Tanggal Produksi Infure',
+            'Shift',
+            'Jam',
+            'Nomor Mesin Infure',
+            'Nomor Han Infure',
+            'Petugas Infure',
+            'Dept. Infure',
+            'Loss Infure di Seitai',
+        ];
+
+        foreach ($header as $key => $value) {
+            $activeWorksheet->setCellValue($columnHeaderEnd . $rowHeaderStart, $value);
+            $columnHeaderEnd++;
+        }
+        $columnHeaderEnd = chr(ord($columnHeaderEnd) - 1);
+
+        // style header
+        phpspreadsheet::addFullBorder($spreadsheet, $columnHeaderStart . $rowHeaderStart . ':' . $columnHeaderEnd . $rowHeaderStart);
+        phpspreadsheet::styleFont($spreadsheet, $columnHeaderStart . $rowHeaderStart . ':' . $columnHeaderEnd . $rowHeaderStart, true, 9, 'Calibri');
+        phpspreadsheet::textAlignCenter($spreadsheet, $columnHeaderStart . $rowHeaderStart . ':' . $columnHeaderEnd . $rowHeaderStart);
+
+        // qeury belum bener
+        $data = collect(DB::select(
+            "
+                SELECT
+                    tod.id,
+                    tod.po_no,
+                    mp.code,
+                    mp.name AS produk_name,
+                    tod.product_code,
+                    tod.order_qty,
+                    tod.order_unit,
+                    tod.stufingdate,
+                    tod.etddate,
+                    tod.etadate,
+                    mbu.NAME AS buyer_name
+                FROM
+                    tdorder AS tod
+                INNER JOIN msproduct AS mp ON mp.id = tod.product_id
+                INNER JOIN msbuyer AS mbu ON mbu.id = tod.buyer_id",
+        ));
+
+        if (count($data) == 0) {
+            $response = [
+                'status' => 'error',
+                'message' => "Data pada periode tanggal atau pembeli tersebut tidak ditemukan"
+            ];
+
+            return $response;
+        }
+
+        $activeWorksheet->getStyle($columnHeaderStart . $rowHeaderStart . ':' . $columnHeaderEnd . $rowHeaderStart)->getAlignment()->setWrapText(true);
+
+        // size auto
+        // $columnSizeStart = $columnItemStart;
+        // $columnSizeStart++;
+        // while ($columnSizeStart !== $columnItemEnd) {
+        //     $spreadsheet->getActiveSheet()->getColumnDimension($columnSizeStart)->setAutoSize(true);
+        //     $columnSizeStart++;
+        // }
+
+        $writer = new Xlsx($spreadsheet);
+        $filename = 'Detail-Produksi-'. $this->nippo . '.xlsx';
+        $writer->save($filename);
+        $response = [
+            'status' => 'success',
+            'filename' => $filename
+        ];
+        return $response;
+    }
+
+
+
     public function render()
     {
         return view('livewire.report.detail-report')->extends('layouts.master');
--- resources/views/livewire/report/detail-report.blade.php
+++ resources/views/livewire/report/detail-report.blade.php
@@ -1,111 +1,127 @@
 <div class="row">
     <div class="col-lg-2"></div>
     <div class="col-lg-6">
-        {{-- Filter tanggal awal --}}
-        <div class="form-group">
-            <label class="control-label col-md-4 col-xs-12 fw-bold">Tanggal Periode</label>
-            <div class="col-12 mt-1">
-                <div class="form-group">
-                    <div class="input-group">
-                        <span class="input-group-addon col-lg-3 text-muted">Awal: </span>
-                        <input wire:model.defer="tglAwal" type="text" class="form-control" style="padding:0.44rem"
-                            data-provider="flatpickr" data-date-format="Y-m-d">
-                        <span class="input-group-text py-0">
-                            <i class="ri-calendar-event-fill fs-4"></i>
-                        </span>
+        <form wire:submit.prevent="export">
+            {{-- Filter tanggal awal --}}
+            <div class="form-group">
+                <label class="control-label col-md-4 col-xs-12 fw-bold">Tanggal Periode</label>
+                <div class="col-12 mt-1">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <span class="input-group-addon col-lg-3 text-muted">Awal: </span>
+                            <input wire:model.defer="tglAwal" type="text" class="form-control"
+                                style="padding:0.44rem" data-provider="flatpickr" data-date-format="Y-m-d">
+                            <span class="input-group-text py-0">
+                                <i class="ri-calendar-event-fill fs-4"></i>
+                            </span>
 
-                        <select wire:ignore wire:model.defer="jamAwal" class="form-control"
-                            placeholder="- pilih jam kerja -">
-                            @foreach ($workingShiftHour as $item)
-                                <option value="{{ $item->work_hour_from }}">{{ $item->work_hour_from }}</option>
-                            @endforeach
-                        </select>
-                        <span class="input-group-text py-0">
-                            <i class="ri-time-line fs-4"></i>
-                        </span>
+                            <select wire:ignore wire:model.defer="jamAwal" class="form-control"
+                                placeholder="- pilih jam kerja -">
+                                @foreach ($workingShiftHour as $item)
+                                    <option value="{{ $item->work_hour_from }}">{{ $item->work_hour_from }}</option>
+                                @endforeach
+                            </select>
+                            <span class="input-group-text py-0">
+                                <i class="ri-time-line fs-4"></i>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        {{-- Filter tanggal akhir --}}
-        <div class="form-group mt-1">
-            <div class="col-12">
-                <div class="form-group">
-                    <div class="input-group">
-                        <span class="input-group-addon col-lg-3 text-muted">Akhir: </span>
-                        <input wire:model.defer="tglAkhir" type="text" class="form-control" style="padding:0.44rem"
-                            data-provider="flatpickr" data-date-format="Y-m-d">
-                        <span class="input-group-text py-0">
-                            <i class="ri-calendar-event-fill fs-4"></i>
-                        </span>
+            {{-- Filter tanggal akhir --}}
+            <div class="form-group mt-1">
+                <div class="col-12">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <span class="input-group-addon col-lg-3 text-muted">Akhir: </span>
+                            <input wire:model.defer="tglAkhir" type="text" class="form-control"
+                                style="padding:0.44rem" data-provider="flatpickr" data-date-format="Y-m-d">
+                            <span class="input-group-text py-0">
+                                <i class="ri-calendar-event-fill fs-4"></i>
+                            </span>
 
-                        <select wire:ignore wire:model.defer="jamAkhir" class="form-control"
-                            placeholder="- pilih jam kerja -">
-                            @foreach ($workingShiftHour as $item)
-                                <option value="{{ $item->work_hour_till }}">{{ $item->work_hour_till }}</option>
-                            @endforeach
-                        </select>
-                        {{-- <input wire:model.defer="jamAkhir" type="text" class="form-control" data-provider="timepickr" data-time-hrs="true" id="timepicker-24hrs"> --}}
-                        <span class="input-group-text py-0">
-                            <i class="ri-time-line fs-4"></i>
-                        </span>
+                            <select wire:ignore wire:model.defer="jamAkhir" class="form-control"
+                                placeholder="- pilih jam kerja -">
+                                @foreach ($workingShiftHour as $item)
+                                    <option value="{{ $item->work_hour_till }}">{{ $item->work_hour_till }}</option>
+                                @endforeach
+                            </select>
+                            {{-- <input wire:model.defer="jamAkhir" type="text" class="form-control" data-provider="timepickr" data-time-hrs="true" id="timepicker-24hrs"> --}}
+                            <span class="input-group-text py-0">
+                                <i class="ri-time-line fs-4"></i>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="form-group mt-1">
-            <div class="input-group">
-                <span class="input-group-addon col-12 col-lg-3">Nippo </span>
-                <select id="department" class="form-control" placeholder="- pilih jenis report -">
-                    <option value="1">Infure</option>
-                    <option value="2">Seitai</option>
-                </select>
+            <div class="form-group mt-1">
+                <div class="input-group">
+                    <span class="input-group-addon col-12 col-lg-3">Nippo </span>
+                    <select id="department" class="form-control" placeholder="- pilih jenis report -"
+                        wire:model.defer="nippo">
+                        <option value="Infure">Infure</option>
+                        <option value="Seitai">Seitai</option>
+                    </select>
+                </div>
             </div>
-        </div>
-        <div class="form-group mt-1">
-            <div class="input-group">
-                <span class="input-group-addon col-12 col-lg-3">Nomor LPK </span>
-                <input type="text" class="form-control" placeholder="000000-000">
+            <div class="form-group mt-1">
+                <div class="input-group">
+                    <span class="input-group-addon col-12 col-lg-3">Nomor LPK </span>
+                    <input type="text" class="form-control" placeholder="000000-000">
+                </div>
             </div>
-        </div>
-        <div class="form-group mt-1">
-            <div class="input-group">
-                <span class="input-group-addon col-12 col-lg-3">Nomor Order </span>
-                <input type="text" class="form-control" placeholder="...">
+            <div class="form-group mt-1">
+                <div class="input-group">
+                    <span class="input-group-addon col-12 col-lg-3">Nomor Order </span>
+                    <input type="text" class="form-control" placeholder="...">
+                </div>
             </div>
-        </div>
-        <div class="form-group mt-1">
-            <div class="input-group">
-                <span class="input-group-addon col-12 col-lg-3">Departemen </span>
-                <select id="department" class="form-control" placeholder="- pilih jenis report -">
-                    {{-- <option value="1">Infure</option>
+            <div class="form-group mt-1">
+                <div class="input-group">
+                    <span class="input-group-addon col-12 col-lg-3">Departemen </span>
+                    <select id="department" class="form-control" placeholder="- pilih jenis report -">
+                        {{-- <option value="1">Infure</option>
 					<option value="2">Seitai</option> --}}
-                </select>
+                    </select>
+                </div>
             </div>
-        </div>
-        <div class="form-group mt-1">
-            <div class="input-group">
-                <span class="input-group-addon col-12 col-lg-3">Mesin </span>
-                <select id="department" class="form-control" placeholder="- pilih jenis report -"
-                    onchange="changeDep(this.value)">
-                    {{-- <option value="1">Infure</option>
+            <div class="form-group mt-1">
+                <div class="input-group">
+                    <span class="input-group-addon col-12 col-lg-3">Mesin </span>
+                    <select id="department" class="form-control" placeholder="- pilih jenis report -"
+                        onchange="changeDep(this.value)">
+                        {{-- <option value="1">Infure</option>
 					<option value="2">Seitai</option> --}}
-                </select>
+                    </select>
+                </div>
             </div>
-        </div>
-        <div class="form-group mt-1">
-            <div class="input-group">
-                <span class="input-group-addon col-12 col-lg-3">Nomor Han </span>
-                <input type="text" class="form-control" placeholder="...">
+            <div class="form-group mt-1">
+                <div class="input-group">
+                    <span class="input-group-addon col-12 col-lg-3">Nomor Han </span>
+                    <input type="text" class="form-control" placeholder="...">
+                </div>
             </div>
-        </div>
-        <hr />
-        <div class="form-group">
-            <label class="control-label col-md-4 col-xs-12"></label>
-            <div class="input-group col-md-8 col-xs-12">
-                <button type="button" class="btn btn-success btn-print" wire:click="export" style="width:99%"><i
-                        class="fa fa-print"></i> Generate Report</button>
+            <hr />
+            <div class="form-group">
+                <label class="control-label col-md-4 col-xs-12"></label>
+                <div class="input-group col-md-8 col-xs-12">
+                    <button type="submit" class="btn btn-success btn-print" style="width:99%">
+                        <span wire:loading.remove wire:target="export">
+                            <i class="ri-printer-line"></i> Generate Report
+                        </span>
+                        <div wire:loading wire:target="export">
+                            <span class="d-flex align-items-center">
+                                <span class="spinner-border flex-shrink-0" role="status">
+                                    <span class="visually-hidden">Loading...</span>
+                                </span>
+                                <span class="flex-grow-1 ms-1">
+                                    Loading...
+                                </span>
+                            </span>
+                        </div>
+                    </button>
+                </div>
             </div>
-        </div>
+        </form>
     </div>
 </div>
