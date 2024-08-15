52235752564f0650b6f7cbe6fd71d7b0180814f6
RafliFerdian25 <ferdianrafli25@gmail.com>
2024-08-15 08:00:15 +0700

    `feat` order report - daftar order

--- app/Http/Livewire/OrderReportController.php
+++ app/Http/Livewire/OrderReportController.php
@@ -7,10 +7,15 @@ use Livewire\Component;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use App\Exports\ProductsExport;
+use App\Helpers\phpspreadsheet;
 use App\Models\MsBuyer;
 use App\Models\MsWorkingShift;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Maatwebsite\Excel\Facades\Excel;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class OrderReportController extends Component
 {
@@ -22,6 +27,16 @@ class OrderReportController extends Component
     public $workingShiftHour;
     public $buyer_id;
     public $filter;
+    public $jenisReport;
+
+    protected $rules = [
+        'tglAwal' => 'required',
+        'tglAkhir' => 'required',
+        'jamAwal' => 'required',
+        'jamAkhir' => 'required',
+        'filter' => 'required',
+        'jenisReport' => 'required',
+    ];
 
     public function mount()
     {
@@ -31,9 +46,11 @@ class OrderReportController extends Component
         $this->workingShiftHour = MsWorkingShift::select('work_hour_from', 'work_hour_till')->where('status', 1)->orderBy('work_hour_from', 'ASC')->get();
         $this->jamAwal = $this->workingShiftHour[0]->work_hour_from;
         $this->jamAkhir = $this->workingShiftHour[count($this->workingShiftHour) - 1]->work_hour_till;
+        $this->filter = 'Tanggal Order';
+        $this->jenisReport = 'Daftar Order';
     }
 
-    public function export()
+    public function print()
     {
         return Excel::download(new OrderReportExport(
             $this->tglAwal,
@@ -41,6 +58,131 @@ class OrderReportController extends Component
             $this->buyer_id,
             $this->filter,
         ), 'order_report.xlsx');
+    }
+
+    public function export()
+    {
+        $rules = [
+            'tglAwal' => 'required',
+            'tglAkhir' => 'required',
+            'jamAwal' => 'required',
+            'jamAkhir' => 'required',
+            'filter' => 'required',
+            'jenisReport' => 'required',
+        ];
+
+        $messages = [
+            'tglAwal.required' => 'Tanggal Awal tidak boleh kosong',
+            'tglAkhir.required' => 'Tanggal Akhir tidak boleh kosong',
+            'jamAwal.required' => 'Jam Awal tidak boleh kosong',
+            'jamAkhir.required' => 'Jam Akhir tidak boleh kosong',
+            'filter.required' => 'Filter tidak boleh kosong',
+            'jenisReport.required' => 'Jenis Report tidak boleh kosong',
+        ];
+
+        $validate = Validator::make([
+            'tglAwal' => $this->tglAwal,
+            'tglAkhir' => $this->tglAkhir,
+            'jamAwal' => $this->jamAwal,
+            'jamAkhir' => $this->jamAkhir,
+            'filter' => $this->filter,
+            'jenisReport' => $this->jenisReport,
+        ], $rules, $messages);
+
+        if ($validate->fails()) {
+            $this->dispatch('notification', ['type' => 'warning', 'message' => $validate->errors()->first()]);
+            return;
+        }
+
+        if ($this->tglAwal > $this->tglAkhir) {
+            session()->flash('error', 'Tanggal akhir tidak boleh kurang dari tanggal awal');
+            return;
+        }
+
+        $tglAwal = Carbon::parse($this->tglAwal . ' ' . $this->jamAwal);
+        $tglAkhir = Carbon::parse($this->tglAkhir . ' ' . $this->jamAkhir);
+
+        switch ($this->jenisReport) {
+            case 'Daftar Order':
+                $file = $this->daftarOrder($tglAwal, $tglAkhir);
+                return response()->download($file);
+                break;
+        }
+    }
+
+    public function daftarOrder($tglAwal, $tglAkhir)
+    {
+        $spreadsheet = new Spreadsheet();
+        $activeWorksheet = $spreadsheet->getActiveSheet();
+        $activeWorksheet->setShowGridlines(false);
+
+        // Set locale agar tanggal indonesia
+        Carbon::setLocale('id');
+
+        // Judul
+        $activeWorksheet->setCellValue('A1', 'ORDER LIST');
+        $activeWorksheet->setCellValue('A2', 'Periode Order: ' . $tglAwal->translatedFormat('d-M-Y') . ' s/d ' . $tglAkhir->translatedFormat('d-M-Y') . ' - Buyer: ' . ($this->buyer_id != null ? MsBuyer::find($this->buyer_id)->name : 'all'));
+        // Style Judul
+        phpspreadsheet::styleFont($spreadsheet, 'A1:A2', true, 11, 'Calibri');
+
+        // header
+        $rowHeaderStart = 3;
+        $columnHeaderStart = 'A';
+        $columnHeaderEnd = 'A';
+        $header = [
+            'No',
+            'Order Data',
+            'PO Number',
+            'Order No',
+            'Product Name',
+            'Type Code',
+            'Order Quantity',
+            'Unit',
+            'Stufing Date',
+            'ETD Date',
+            'ETA Date',
+        ];
+
+        foreach ($header as $key => $value) {
+            $activeWorksheet->setCellValue($columnHeaderEnd . $rowHeaderStart, $value);
+            $columnHeaderEnd++;
+        }
+        $columnHeaderEnd = chr(ord($columnHeaderEnd) - 1);
+
+        // style header
+        phpspreadsheet::addFullBorder($spreadsheet, $columnHeaderStart . $rowHeaderStart . ':' . $columnHeaderEnd . $rowHeaderStart);
+        phpspreadsheet::styleFont($spreadsheet, $columnHeaderStart . $rowHeaderStart . ':' . $columnHeaderEnd . $rowHeaderStart, true, 9, 'Calibri');
+        phpspreadsheet::textAlignCenter($spreadsheet, $columnHeaderStart . $rowHeaderStart . ':' . $columnHeaderEnd . $rowHeaderStart);
+
+        $data = collect(DB::select("
+            SELECT
+                tod.id,
+                tod.order_date,
+                tod.po_no,
+                mp.code,
+                mp.name AS produk_name,
+                tod.product_code,
+                tod.order_qty,
+                tod.order_unit,
+                tod.stufingdate,
+                tod.etddate,
+                tod.etadate,
+                mbu.NAME AS buyer_name
+            FROM
+                tdorder AS tod
+            INNER JOIN msproduct AS mp ON mp.id = tod.product_id
+            INNER JOIN msbuyer AS mbu ON mbu.id = tod.buyer_id
+            "));
+        // $tglMasuk
+        // $tglKeluar
+        // $buyer_id
+
+        $activeWorksheet->getStyle($columnHeaderStart . $rowHeaderStart . ':' . $columnHeaderEnd . $rowHeaderStart)->getAlignment()->setWrapText(true);
+
+        $writer = new Xlsx($spreadsheet);
+        $filename = 'Order-List.xlsx';
+        $writer->save($filename);
+        return $filename;
     }
 
     public function render()
--- resources/views/livewire/order-lpk/order-report.blade.php
+++ resources/views/livewire/order-lpk/order-report.blade.php
@@ -1,125 +1,127 @@
 <div class="container">
-	<div class="row">
-		<div class="col-lg-2"></div>
-		<div class="col-lg-6">
-            {{-- Filter tanggal awal --}}
-            <div class="form-group">
-                <label class="control-label col-md-4 col-xs-12 fw-bold">Tanggal Periode</label>
-                <div class="col-12 mt-1">
-                    <div class="form-group">
-                        <div class="input-group">
-                            <span class="input-group-addon col-3 fw-bold">Awal: </span>
-                            <input wire:model.defer="tglAwal" type="text" class="form-control" style="padding:0.44rem"
-                                data-provider="flatpickr" data-date-format="Y-m-d">
-                            <span class="input-group-text py-0">
-                                <i class="ri-calendar-event-fill fs-4"></i>
-                            </span>
+    <div class="row">
+        <div class="col-lg-2"></div>
+        <div class="col-lg-6">
+            <form wire:submit.prevent="export">
+                {{-- Filter tanggal awal --}}
+                <div class="form-group">
+                    <label class="control-label col-md-4 col-xs-12 fw-bold">Tanggal Periode</label>
+                    <div class="col-12 mt-1">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon col-3 fw-bold">Awal: </span>
+                                <input wire:model.defer="tglAwal" type="text" class="form-control"
+                                    style="padding:0.44rem" data-provider="flatpickr" data-date-format="Y-m-d">
+                                <span class="input-group-text py-0">
+                                    <i class="ri-calendar-event-fill fs-4"></i>
+                                </span>
 
-                            <select wire:ignore wire:model.defer="jamAwal" class="form-control"
-                                placeholder="- pilih jam kerja -">
-                                @foreach ($workingShiftHour as $item)
-                                    <option value="{{ $item->work_hour_from }}">{{ $item->work_hour_from }}</option>
-                                @endforeach
-                            </select>
-                            <span class="input-group-text py-0">
-                                <i class="ri-time-line fs-4"></i>
-                            </span>
+                                <select wire:ignore wire:model.defer="jamAwal" class="form-control"
+                                    placeholder="- pilih jam kerja -">
+                                    @foreach ($workingShiftHour as $item)
+                                        <option value="{{ $item->work_hour_from }}">{{ $item->work_hour_from }}</option>
+                                    @endforeach
+                                </select>
+                                <span class="input-group-text py-0">
+                                    <i class="ri-time-line fs-4"></i>
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            {{-- Filter tanggal akhir --}}
-            <div class="form-group mt-1">
-                <div class="col-12">
-                    <div class="form-group">
-                        <div class="input-group">
-                            <span class="input-group-addon col-3 fw-bold">Akhir: </span>
-                            <input wire:model.defer="tglAkhir" type="text" class="form-control" style="padding:0.44rem"
-                                data-provider="flatpickr" data-date-format="Y-m-d">
-                            <span class="input-group-text py-0">
-                                <i class="ri-calendar-event-fill fs-4"></i>
-                            </span>
+                {{-- Filter tanggal akhir --}}
+                <div class="form-group mt-1">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon col-3 fw-bold">Akhir: </span>
+                                <input wire:model.defer="tglAkhir" type="text" class="form-control"
+                                    style="padding:0.44rem" data-provider="flatpickr" data-date-format="Y-m-d">
+                                <span class="input-group-text py-0">
+                                    <i class="ri-calendar-event-fill fs-4"></i>
+                                </span>
 
-                            <select wire:ignore wire:model.defer="jamAkhir" class="form-control"
-                                placeholder="- pilih jam kerja -">
-                                @foreach ($workingShiftHour as $item)
-                                    <option value="{{ $item->work_hour_till }}">{{ $item->work_hour_till }}</option>
-                                @endforeach
-                            </select>
-                            {{-- <input wire:model.defer="jamKeluar" type="text" class="form-control" data-provider="timepickr" data-time-hrs="true" id="timepicker-24hrs"> --}}
-                            <span class="input-group-text py-0">
-                                <i class="ri-time-line fs-4"></i>
-                            </span>
+                                <select wire:ignore wire:model.defer="jamAkhir" class="form-control"
+                                    placeholder="- pilih jam kerja -">
+                                    @foreach ($workingShiftHour as $item)
+                                        <option value="{{ $item->work_hour_till }}">{{ $item->work_hour_till }}</option>
+                                    @endforeach
+                                </select>
+                                {{-- <input wire:model.defer="jamKeluar" type="text" class="form-control" data-provider="timepickr" data-time-hrs="true" id="timepicker-24hrs"> --}}
+                                <span class="input-group-text py-0">
+                                    <i class="ri-time-line fs-4"></i>
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-			<div class="form-group mt-1">
-				<div class="col-12">
-					<div class="form-group">
-						<div class="input-group">
-							<span class="input-group-addon col-3 fw-bold">Filter:&nbsp;</span>
-							<select class="form-control" wire:model.defer="filter">
-								<option value="1">Tanggal Order</option>
-								<option value="2">Tanggal Proses</option>
-							</select>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="form-group mt-1">
-				<div class="col-12">
-					<div class="form-group">
-						<div class="input-group">
-							<span class="input-group-addon col-3 fw-bold">Buyer</span>
-							<select class="form-control" wire:model.defer="buyer_id">
-								<option value=""> -- ALL --</option>
-								@foreach ($buyer as $item)
-									<option value="{{ $item->id }}">{{ $item->name }}</option>
-								@endforeach
-							</select>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="form-group mt-1">
-				<div class="col-12">
-					<div class="form-group">
-						<div class="input-group">
-							<span class="input-group-addon col-3 fw-bold">Jenis Report</span>
-							<select class="form-control">
-								<option value=""> -- ALL --</option>
-								<option value="1">Daftar Order</option>
-								<option value="2">Daftar Order Per Buyer Per Tipe</option>
-								<option value="3">CheckList Order</option>
-								<option value="4">CheckList LPK</option>
-								<option value="5">Progress Order</option>
-							</select>
-						</div>
-					</div>
-				</div>
-			</div>
-			<hr />
-			<div class="form-group">
-				<div class="input-group">
-					<button type="button" class="btn btn-success btn-print" wire:click="export" style="width:99%">
-						<span wire:loading.remove wire:target="export">
-							<i class="ri-printer-line"></i> Generate Report
-						</span>
-						<div wire:loading wire:target="export">
-							<span class="d-flex align-items-center">
-								<span class="spinner-border flex-shrink-0" role="status">
-									<span class="visually-hidden">Loading...</span>
-								</span>
-								<span class="flex-grow-1 ms-1">
-									Loading...
-								</span>
-							</span>
-						</div>
-					</button>
-				</div>
-			</div>
-		</div>
-		<div class="col-lg-4"></div>
-	</div>
+                <div class="form-group mt-1">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <div class="input-group" wire:ignore>
+                                <span class="input-group-addon col-3 fw-bold">Filter:&nbsp;</span>
+                                <select class="form-control" wire:model.defer="filter">
+                                    <option selected value="Tanggal Order">Tanggal Order</option>
+                                    <option value="Tanggal Proses">Tanggal Proses</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group mt-1">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <div class="input-group" wire:ignore>
+                                <span class="input-group-addon col-3 fw-bold">Buyer</span>
+                                <select class="form-control" wire:model.defer="buyer_id">
+                                    <option value=""> -- ALL --</option>
+                                    @foreach ($buyer as $item)
+                                        <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group mt-1">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <div class="input-group" wire:ignore>
+                                <span class="input-group-addon col-3 fw-bold">Jenis Report</span>
+                                <select class="form-control" wire:model.defer="jenisReport">
+                                    <option value="Daftar Order" selected>Daftar Order</option>
+                                    <option value="Daftar Order Per Buyer Per Tipe">Daftar Order Per Buyer Per Tipe
+                                    </option>
+                                    <option value="CheckList Order">CheckList Order</option>
+                                    <option value="CheckList LPK">CheckList LPK</option>
+                                    <option value="Progress Order">Progress Order</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div class="form-group">
+                    <div class="input-group">
+                        <button type="submit" class="btn btn-success btn-print" style="width:99%">
+                            <span wire:loading.remove wire:target="export">
+                                <i class="ri-printer-line"></i> Generate Report
+                            </span>
+                            <div wire:loading wire:target="export">
+                                <span class="d-flex align-items-center">
+                                    <span class="spinner-border flex-shrink-0" role="status">
+                                        <span class="visually-hidden">Loading...</span>
+                                    </span>
+                                    <span class="flex-grow-1 ms-1">
+                                        Loading...
+                                    </span>
+                                </span>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-lg-4"></div>
+    </div>
 </div>
