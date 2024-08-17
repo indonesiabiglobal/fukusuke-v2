77ea4be8cae6488b6b7bff579f842ca0555cc4ac
RafliFerdian25 <ferdianrafli25@gmail.com>
2024-08-17 20:01:04 +0700

    `fix` view form report kenpin

--- app/Http/Livewire/Kenpin/ReportKenpinController.php
+++ app/Http/Livewire/Kenpin/ReportKenpinController.php
@@ -3,30 +3,43 @@
 namespace App\Http\Livewire\Kenpin;
 
 use App\Exports\KenpinExport;
+use App\Models\MsDepartment;
+use App\Models\MsWorkingShift;
 use Carbon\Carbon;
 use Livewire\Component;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ReportKenpinController extends Component
 {
-    public $tglMasuk;
-    public $tglKeluar;
+    public $tglAwal;
+    public $tglAkhir;
+    public $jamAwal;
+    public $jamAkhir;
+    public $workingShiftHour;
+    public $department;
+    public $departmentId;
     public $buyer;
     public $buyer_id;
-    public $filter;
+    public $lpk_no;
+    public $noorder;
+    public $nomorKenpin;
+    public $nomorHan;
+    public $status;
 
     public function mount()
     {
-        // $this->tglMasuk = Carbon::now()->format('Y-m-d');
-        $this->tglMasuk = Carbon::now()->format('Y-m-d\T00:00');
-        $this->tglKeluar = Carbon::now()->format('Y-m-d\T23:59');
-        // $this->buyer = MsBuyer::get();      
+        $this->tglAwal = Carbon::now()->format('Y-m-d');
+        $this->tglAkhir = Carbon::now()->format('Y-m-d');
+        $this->workingShiftHour = MsWorkingShift::select('work_hour_from', 'work_hour_till')->where('status', 1)->orderBy('work_hour_from', 'ASC')->get();
+        $this->jamAwal = $this->workingShiftHour[0]->work_hour_from;
+        $this->jamAkhir = $this->workingShiftHour[count($this->workingShiftHour) - 1]->work_hour_till;
+        $this->department = MsDepartment::get();
     }
 
     public function export()
     {
         // return Excel::download(new KenpinExport(
-        //     $this->tglMasuk, 
+        //     $this->tglMasuk,
         //     $this->tglKeluar
         // ), 'Kenpin_report.xlsx');
     }
--- resources/views/livewire/kenpin/report-kenpin.blade.php
+++ resources/views/livewire/kenpin/report-kenpin.blade.php
@@ -1,80 +1,133 @@
 <div class="row mt-3">
-	<div class="col-lg-2"></div>
-	<div class="col-lg-6">
-		<div class="form-group">
-			<label class="control-label col-md-4 col-xs-12">Tanggal Kenpin</label>
-			<div class="col-12 mt-1">
-				<div class="form-group">
-					<div class="input-group">
-						<span class="input-group-addon col-12 col-lg-2">Awal: </span>
-						<input class="form-control datepicker-input" type="datetime-local" wire:model.defer="tglMasuk" placeholder="yyyy/mm/dd hh:mm" />
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="form-group mt-1">
-			<div class="col-12">
-				<div class="form-group">
-					<div class="input-group">
-						<span class="input-group-addon col-12 col-lg-2">Akhir: </span>
-						<input class="form-control datepicker-input" type="datetime-local" wire:model.defer="tglKeluar" placeholder="yyyy/mm/dd hh:mm" />
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="form-group mt-1">
-			<div class="input-group">
-				<span class="input-group-addon col-12 col-lg-3">Departement</span>
-				<select class="form-control">
-					<option value="">- all -</option>
-					<option value="1"></option>
-					<option value="2"></option>
-				</select>
-			</div>
-		</div>
-		<div class="form-group mt-1">
-			<div class="input-group">
-				<span class="input-group-addon col-12 col-lg-3">Nomor LPK </span>
-				<input type="text" class="form-control" placeholder="000000-000">
-			</div>
-		</div>
+    <div class="col-lg-2"></div>
+    <div class="col-lg-6">
+        <div class="form-group">
+            <label class="control-label col-md-4 col-xs-12">Tanggal Kenpin</label>
+            <div class="col-12 mt-1">
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon col-12 col-lg-2">Awal: </span>
+                        <input wire:model.defer="tglAwal" type="text" class="form-control" style="padding:0.44rem"
+                            data-provider="flatpickr" data-date-format="Y-m-d">
+                        <span class="input-group-text py-0">
+                            <i class="ri-calendar-event-fill fs-4"></i>
+                        </span>
+
+                        <div class="mb-1" wire:ignore>
+                            <select class="form-control" wire:model.defer="jamAwal" data-choices
+                                data-choices-sorting-false data-choices-removeItem>
+                                <option value="">- All -</option>
+                                @foreach ($workingShiftHour as $item)
+                                    <option value="{{ $item->work_hour_from }}"
+                                        @if ($jamAwal == $item->work_hour_from) selected @endif>{{ $item->work_hour_from }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <span class="input-group-text py-0">
+                            <i class="ri-time-line fs-4"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="form-group mt-1">
-			<div class="input-group">
-				<span class="input-group-addon col-12 col-lg-3">Nomor Order </span>
-				<input type="text" class="form-control" placeholder="..">
-			</div>
-		</div>
+            <div class="col-12">
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon col-12 col-lg-2">Akhir: </span>
+                        <input wire:model.defer="tglAkhir" type="text" class="form-control" style="padding:0.44rem"
+                            data-provider="flatpickr" data-date-format="Y-m-d">
+                        <span class="input-group-text py-0">
+                            <i class="ri-calendar-event-fill fs-4"></i>
+                        </span>
+
+                        <div class="mb-1" wire:ignore>
+                            <select class="form-control" wire:model.defer="jamAkhir" data-choices
+                                data-choices-sorting-false data-choices-removeItem>
+                                <option value="">- All -</option>
+                                @foreach ($workingShiftHour as $item)
+                                    <option value="{{ $item->work_hour_till }}"
+                                        @if ($jamAkhir == $item->work_hour_till) selected @endif>{{ $item->work_hour_till }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <span class="input-group-text py-0">
+                            <i class="ri-time-line fs-4"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="form-group mt-1">
-			<div class="input-group">
-				<span class="input-group-addon col-12 col-lg-3">Nomor Kenpin </span>
-				<input type="text" class="form-control" placeholder="_____-_____">
-			</div>
-		</div>
+            <div class="row">
+                <div class="col-12 col-lg-3">
+                    <label for="product" class="form-label text-muted">Departemen</label>
+                </div>
+                <div class="col-12 col-lg-9">
+                    <div class="mb-1" wire:ignore>
+                        <select class="form-control" wire:model.defer="departmentId" data-choices
+                            data-choices-sorting-false data-choices-removeItem>
+                            <option value="">- All -</option>
+                            @foreach ($department as $item)
+                                <option value="{{ $item->id }}">{{ $item->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="form-group mt-1">
-			<div class="input-group">
-				<span class="input-group-addon col-12 col-lg-3">Nomor Han </span>
-				<input type="text" class="form-control" placeholder="00-00-00A">
-			</div>
-		</div>
-		<div class="form-group mt-1">
-			<div class="input-group">
-				<span class="input-group-addon col-12 col-lg-3">Status</span>
-				<select class="form-control">
-					<option value="">- all -</option>
-					<option value="1">Proses</option>
-					<option value="2">Finish</option>
-				</select>
-			</div>
-		</div>
-		<hr />
-		<div class="form-group">
-			<label class="control-label col-md-4 col-xs-12"></label>
-			<div class="input-group col-md-8 col-xs-12">
-				<button type="button" class="btn btn-success btn-print" wire:click="export" style="width:99%">
-					<i class="fa fa-print"></i> Generate Report
-				</button>
-			</div>
-		</div>
-	</div>
-	<div class="col-lg-4"></div>
+            <div class="input-group">
+                <span class="input-group-addon col-12 col-lg-3">Nomor LPK </span>
+                <input type="text" class="form-control" placeholder="000000-000" wire:model.defer="lpk_no">
+            </div>
+        </div>
+        <div class="form-group mt-1">
+            <div class="input-group">
+                <span class="input-group-addon col-12 col-lg-3">Nomor Order </span>
+                <input type="text" class="form-control" placeholder=".." wire:model.defer="noorder">
+            </div>
+        </div>
+        <div class="form-group mt-1">
+            <div class="input-group">
+                <span class="input-group-addon col-12 col-lg-3">Nomor Kenpin </span>
+                <input type="text" class="form-control" placeholder="_____-_____" wire:model.defer="nomorKenpin">
+            </div>
+        </div>
+        <div class="form-group mt-1">
+            <div class="input-group">
+                <span class="input-group-addon col-12 col-lg-3">Nomor Han </span>
+                <input type="text" class="form-control" placeholder="00-00-00A" wire:model.defer="nomorHan">
+            </div>
+        </div>
+        <div class="form-group mt-1">
+            <div class="input-group">
+                <span class="input-group-addon col-12 col-lg-3">Status</span>
+                <select class="form-control" wire:model.defer="status">
+                    <option value="">- all -</option>
+                    <option value="proses">Proses</option>
+                    <option value="finish">Finish</option>
+                </select>
+            </div>
+        </div>
+        <hr />
+        <button type="button" class="btn btn-success btn-print" wire:click="export" style="width:99%">
+            <span wire:loading.remove wire:target="export">
+                <i class="ri-printer-line"></i> Generate Report
+            </span>
+            <div wire:loading wire:target="export">
+                <span class="d-flex align-items-center">
+                    <span class="spinner-border flex-shrink-0" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </span>
+                    <span class="flex-grow-1 ms-1">
+                        Loading...
+                    </span>
+                </span>
+            </div>
+        </button>
+    </div>
+    <div class="col-lg-4"></div>
 </div>
