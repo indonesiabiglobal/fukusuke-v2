c0e77f15f839aab2a1dee6b42a43f49e79591702
RafliFerdian25 <ferdianrafli25@gmail.com>
2024-08-18 10:47:16 +0700

    `fix`(version app)

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,11 +3,11 @@
         <div class="row">
             <div class="col-sm-6">
                 © Production Control Management System
-                
+
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    PRODUCTION CONTROL V1.0.8 (build 0), © 2016-<script>document.write(new Date().getFullYear())</script>
+                    PRODUCTION CONTROL V2.0.0 (build 0), © 2016-<script>document.write(new Date().getFullYear())</script>
                 </div>
             </div>
         </div>
