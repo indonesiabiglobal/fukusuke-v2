3e339ca569f0e078f89cb60b16db7cc17780232f
RafliFerdian25 <ferdianrafli25@gmail.com>
2024-08-20 21:23:18 +0700

    `fix` import excel order entry

--- app/Exports/OrderEntryExport.php
+++ app/Exports/OrderEntryExport.php
@@ -3,12 +3,15 @@
 namespace App\Exports;
 
 use App\Models\TdOrder;
+use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 // use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class OrderEntryExport implements FromCollection, WithHeadings
+class OrderEntryExport implements FromCollection, WithHeadings, WithColumnFormatting
 {
     // public function __construct()
     // {
@@ -19,15 +22,15 @@ class OrderEntryExport implements FromCollection, WithHeadings
     {
         return new Collection([
             [
-                'TG_PROSES' => 15032018, 
-                'PO_NUMBER' => 'PO001', 
-                'TG_ORDER' => '14032018',
+                'TG_PROSES' => Carbon::now()->format('d-m-Y'),
+                'PO_NUMBER' => 'PO001',
+                'TG_ORDER' => Carbon::now()->format('d-m-Y'),
                 'NO_ORDER' => 'UH244R1',
                 'QTY_ORDER' => '12',
                 'UNIT' => '1',
-                'TG_STUFING' => '16032018',
-                'TG_ETD' => '16032018',
-                'TG_ETA' => '16032018',
+                'TG_STUFING' => Carbon::now()->format('d-m-Y'),
+                'TG_ETD' => Carbon::now()->format('d-m-Y'),
+                'TG_ETA' => Carbon::now()->format('d-m-Y'),
                 'KODE_BUYER' => '1001',
             ],
         ]);
@@ -38,6 +41,17 @@ class OrderEntryExport implements FromCollection, WithHeadings
         return [
             'TG_PROSES','PO_NUMBER','TG_ORDER','NO_ORDER','QTY_ORDER','UNIT','TG_STUFING','TG_ETD',
             'TG_ETA','KODE_BUYER'
+        ];
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'A' => NumberFormat::FORMAT_DATE_DDMMYYYY,
+            'C' => NumberFormat::FORMAT_DATE_DDMMYYYY,
+            'G' => NumberFormat::FORMAT_DATE_DDMMYYYY,
+            'H' => NumberFormat::FORMAT_DATE_DDMMYYYY,
+            'I' => NumberFormat::FORMAT_DATE_DDMMYYYY,
         ];
     }
 }
