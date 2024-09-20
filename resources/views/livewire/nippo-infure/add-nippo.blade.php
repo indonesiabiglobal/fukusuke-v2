07afcc35cc8caedbf1c4076efff841c1d66ba412
RafliFerdian25 <ferdianrafli25@gmail.com>
2024-09-21 01:20:56 +0700

    `fix`

--- resources/views/livewire/nippo-infure/add-nippo.blade.php
+++ resources/views/livewire/nippo-infure/add-nippo.blade.php
@@ -22,7 +22,7 @@
                         <div class="form-group">
                             <div class="input-group">
                                 <label class="control-label col-5 pe-2">Tanggal Proses</label>
-                                <input class="form-control bg-light" readonly="readonly" type="text" style="padding:0.44rem" data-provider="flatpickr" data-date-format="d/m/Y" wire:model.defer="created_on" placeholder="yyyy/mm/dd"/>
+                                <input class="form-control bg-light" readonly="readonly" disabled type="text" style="padding:0.44rem" data-provider="flatpickr" data-date-format="d/m/Y" wire:model.defer="created_on" placeholder="yyyy/mm/dd"/>
                                 <span class="input-group-text py-0">
                                     <i class="ri-calendar-event-fill fs-4"></i>
                                 </span>
