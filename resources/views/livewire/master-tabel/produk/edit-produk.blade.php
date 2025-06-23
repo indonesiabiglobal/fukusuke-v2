b28eeac7e3d8633f81ee933ed2f9da6596a1615d
RafliFerdian25 <ferdianrafli25@gmail.com>
2025-06-23 12:53:37 +0700

    fix: update product type selection to include search functionality

--- resources/views/livewire/master-tabel/produk/edit-produk.blade.php
+++ resources/views/livewire/master-tabel/produk/edit-produk.blade.php
@@ -29,7 +29,7 @@
                         <div class="input-group">
                             <label class="control-label col-12 col-lg-4">Kode Tipe</label>
                             <div class="col-12 col-lg-8" wire:ignore>
-                                <select required data-choices data-choices-sorting="true"
+                                <select required data-choices data-choices-search-field
                                     class="form-select col-12 col-lg-8 @error('product_type_id') is-invalid @enderror"
                                     wire:model="product_type_id" placeholder="">
                                     <option value="" selected>
