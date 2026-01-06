c43cbf09b61ed98c74c3cb447db982d2c7627498
RafliFerdian25 <ferdianrafli25@gmail.com>
2026-01-06 21:08:25 +0700

    refactor: Clean up commented CSS links in head-css.blade.php

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -18,9 +18,8 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@3.5.1/select2.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
 
-<!--datatable css-->
-{{-- <link href="https://cdn.datatables.net/v/bs5/jq-3.7.0/dt-2.1.4/datatables.min.css" rel="stylesheet"> --}}
+{{-- <link href="{{ URL::asset('build/libs/datatables/css/datatables.min.css') }}" rel="stylesheet"> --}}
 {{-- Select2 --}}
-{{-- <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@3.5.1/select2.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" /> --}}
+{{-- <link href="{{ URL::asset('build/libs/select2/css/select2.min.css') }}" rel="stylesheet" />
+<link rel="stylesheet" href="{{ URL::asset('build/libs/select2/css/select2-3.5.1.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('build/libs/select2/css/select2-bootstrap-5-theme.min.css') }}" /> --}}
